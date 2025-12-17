38160e91a984316689c1ad899a079b6af7f34096
sham-404 <shamsujith4@gmail.com>
2025-12-17 15:21:23 +0530

    Made working prototype

--- src/eval.rs
+++ src/eval.rs
@@ -34,16 +34,31 @@ pub fn to_postfix(infix: Vec<&str>) -> Vec<&str> {
     res
 }
 
-pub fn _eval_postfix(postfix: &[&str]) -> f64 {
+pub fn eval_postfix(postfix: &[&str]) -> f64 {
     let mut stack: Vec<f64> = Vec::new();
 
     for &ch in postfix {
         if !is_operator(ch) {
+            stack.push(ch.parse::<f64>().unwrap());
         } else {
+            let a = stack.pop().unwrap();
+            let b = stack.pop().unwrap();
+
+            stack.push(compute(a, b, ch));
         }
     }
+    stack.pop().unwrap()
+}
 
-    0.0
+fn compute(a: f64, b: f64, ch: &str) -> f64 {
+    match ch {
+        "^" => b.powf(a),
+        "*" => b * a,
+        "/" => b / a,
+        "+" => b + a,
+        "-" => b - a,
+        _ => 0.0,
+    }
 }
 
 fn precedence(c: &str) -> i16 {
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod eval;
+use eval::eval_postfix;
 use eval::to_postfix;
 
 fn main() {
-    println!(
-        "{:?}",
-        to_postfix(vec!["23", "+", "5", "/", "(", "45.2", "-", "90", ")"])
-    );
+    let a = to_postfix(vec!["23", "+", "5", "/", "(", "45.2", "-", "90", ")"]);
+
+    println!("{:?}", a);
+    println!("{}", eval_postfix(&a));
+    println!("{}", eval_postfix(&to_postfix(vec!["4", "+", "5"])));
 }
