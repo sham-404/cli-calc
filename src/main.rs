4c9f8deec92639c2b00e67b6c8adc71371c3dfe8
sham-404 <shamsujith4@gmail.com>
2026-03-08 21:43:55 +0530

    Small fix

--- src/main.rs
+++ src/main.rs
@@ -51,8 +51,6 @@ fn calculate(exp: &String, param: &str) {
     };
 
     let postfix = to_postfix(&parsed);
-
-    println!("Postfix: {postfix:?}");
     let res = eval_postfix(&postfix);
 
     println!("{res}");
